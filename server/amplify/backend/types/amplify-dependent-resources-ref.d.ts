f0feaa64b1e13864e78b94438c869936b559433b
simden012 <denis.simon1211@gmail.com>
2024-01-20 15:42:33 -0500

    amplify graphql

---
+++ server/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "aihub": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
